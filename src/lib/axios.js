ac5f1dc1c5acc7b9f12403b6283ad8c54ae3fdc6
NikithaDevasoth <devasothnikitha09@gmail.com>
2025-09-04 19:55:27 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "https://notes-backend-3-bmmh.onrender.com/api" : "/api";
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export default api;
